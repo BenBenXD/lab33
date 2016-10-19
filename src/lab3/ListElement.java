d8b75267e3af87386e37fd52427d60c908949a2b
BenBenXD <LOllipopXDD@hotmail.com>
2016-10-19 04:51:17 -0600

    first commit

---
+++ src/lab3/ListElement.java
@@ -0,0 +1,28 @@
+package lab3;
+
+public class ListElement
+{
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+
+	public ListElement() 
+	{
+		this.data=0;
+		this.next=null;
+		this.previous=null;
+	}
+		/**
+		 * @param data
+		 * an integer to sorted in the node
+		 * @return 
+		 * none
+		 */
+	public void setData(int data)
+	{
+		this.data=data;
+	}
+	public int getdata() {
+		return this.data;
+	}
+}
